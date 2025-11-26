bf877865e3a2f0e00a8384b0fc783d6ef22192bb
unity <87710132+incxs2@users.noreply.github.com>
2025-11-26 08:38:54 +0000

    Initial commit

---
+++ eslint.config.mjs
@@ -0,0 +1,8 @@
+import js from "@eslint/js";
+import globals from "globals";
+import { defineConfig } from "eslint/config";
+import eslintConfigPrettier from "eslint-config-prettier/flat";
+
+export default defineConfig([
+  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended", "prettier"], languageOptions: { globals: globals.browser } },
+]);
